60b24dff3c1d0d64aa13c18e997c8c29506b3980
edaild <ch5183sb@gmail.com>
2026-01-04 11:41:51 +0900

    [Feat] 궁극기 영상 변경

--- Assets/Scripts/Player/SkillBall.cs
+++ Assets/Scripts/Player/SkillBall.cs
@@ -10,8 +10,6 @@ public class SkillBall : MonoBehaviour
     public float BallRotageSpeed;
     public float BallDamage;
 
-
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //if (collision.gameObject.CompareTag("Ememi"))
