1828f0f1ac904656e1589aa9301b3ab0521ed98a
edaild <ch5183sb@gmail.com>
2026-01-08 00:27:21 +0900

    [Fix] 궁극기 영상 재연결

--- Assets/Scripts/GameSystem/SpawnSystem.cs
+++ Assets/Scripts/GameSystem/SpawnSystem.cs
@@ -11,6 +11,8 @@ public class SpawnSystem : MonoBehaviour
     public QuestSystem questSystem;
     public bool isPlayer;
 
+    private bool isNullObjectScene;
+
     private void Awake()
     {
         if(instance == null)
@@ -24,38 +26,55 @@ public class SpawnSystem : MonoBehaviour
 
     private void Start()
     {
-        PlayerSapwn();
+        //PlayerSapwn();
 
         if(questSystem == null)
             questSystem = GetComponent<QuestSystem>();
     }
 
-    void PlayerSapwn()
+    private void OnEnable()
     {
-        string isSpawnPoint = "";
-
-        if(questSystem.playerquerstID <= 10005 || questSystem.playerLevel <= 2 || SceneManager.GetActiveScene().name == "Snowvillage" || !isPlayer)
-        {
-            isSpawnPoint = "눈의 마을 스폰 포인트";
-            Debug.Log("눈의 마을 스폰");
-        }
-
-        if (questSystem.playerquerstID >= 10005 || questSystem.playerLevel >= 2)
-        {
-            isSpawnPoint = "아이시클 시티";
-            Debug.Log("아이시클 시티 스폰 포인트");
-        }
-
-        if (string.IsNullOrEmpty(isSpawnPoint)) return;
-
-        Transform targetSpawnPoint = spawnPont.Find(s => s.name == isSpawnPoint);
-
-        if(targetSpawnPoint != null)
-        {
-            Instantiate(playerPrefab,targetSpawnPoint.transform.position, Quaternion.identity);
-            Debug.Log($"{targetSpawnPoint} 으로 플레이어 생성");
-            isPlayer = true;
-        } 
-
+        SceneManager.sceneLoaded += IsNotInGameScene;
     }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= IsNotInGameScene;
+    }
+
+    void IsNotInGameScene(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "LumiHouseScene" || scene.name == "MaigicurlHotal" || scene.name == "LobbyScene")
+            Destroy(gameObject);
+      
+    }
+
+    //void PlayerSapwn()
+    //{
+    //    string isSpawnPoint = "";
+
+    //    if(questSystem.playerquerstID <= 10005 || questSystem.playerLevel <= 2 || SceneManager.GetActiveScene().name == "Snowvillage" || !isPlayer)
+    //    {
+    //        isSpawnPoint = "눈의 마을 스폰 포인트";
+    //        Debug.Log("눈의 마을 스폰");
+    //    }
+
+    //    if (questSystem.playerquerstID >= 10005 || questSystem.playerLevel >= 2)
+    //    {
+    //        isSpawnPoint = "아이시클 시티";
+    //        Debug.Log("아이시클 시티 스폰 포인트");
+    //    }
+
+    //    if (string.IsNullOrEmpty(isSpawnPoint)) return;
+
+    //    Transform targetSpawnPoint = spawnPont.Find(s => s.name == isSpawnPoint);
+
+    //    if(targetSpawnPoint != null)
+    //    {
+    //        Instantiate(playerPrefab,targetSpawnPoint.transform.position, Quaternion.identity);
+    //        Debug.Log($"{targetSpawnPoint} 으로 플레이어 생성");
+    //        isPlayer = true;
+    //    } 
+
+    //}
 }
